bdf0f27bc830c160cbccbdd6eeda775d0daf6d3c
JuliaBel5 <juliabel5@gmail.com>
2023-02-01 21:09:42 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+
+    let number = Math.abs(n);
+    let arr = Array.from(number.toString(), Number)
+    arr.reverse();
+    let result = arr.join('');
+    
+return(result);
+
 }
+
